cd3fd1418cad7f68e4c5412418ea0ed4d973f085
Md. Soyeb Islam <132276374+soyeb101@users.noreply.github.com>
2023-08-07 18:16:08 +0600

    push code

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Select, SelectOption } from "./Select";
+import { useState } from "react";
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 },
+];
+
+const App = () => {
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
+
+  return (
+    <>
+      <Select
+        multiple
+        options={options}
+        value={value1}
+        onChange={(o) => setValue1(o)}
+      />
+      <br />
+      <Select options={options} value={value2} onChange={(o) => setValue2(o)} />
+    </>
+  );
+};
+
+export default App;
